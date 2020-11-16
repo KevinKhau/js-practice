f2e9ed3442fed30cf3ae149b79cbbcd5f4ac9382
Kévin Khau <kkhau@Agito>
2020-11-16 21:40:55 +0100

    TDD practice project init

---
+++ functional-programming.js
@@ -0,0 +1,27 @@
+/**
+ * For every given odd number, output the square, sorted in decreasing order.
+ */
+
+let inputs = [
+    [0],
+    [1, 2, 3, 4],
+    [2, 3, 5, 9],
+    [4, 4, 8, 2, 1, 1, 0]
+]
+
+let expectedOutputs = [
+    [],
+    [9, 1],
+    [81, 25, 9],
+    [1]
+]
+
+function tests() {
+
+}
+
+// Complete method below :
+function compute(input) {
+}
+
+tests();
